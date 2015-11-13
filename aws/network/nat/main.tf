4fdd08d1fdbc1b8ce4b29d76d2258fde2cae842b
Anton Babenko <anton@telenordigital.com>
2015-11-13 17:40:26 +0100

    AWS NAT fixed

--- aws/network/nat/main.tf
+++ aws/network/nat/main.tf
@@ -7,8 +7,6 @@ variable "vpc_id" {}
 variable "vpc_cidr" {}
 variable "subnet_ids" {}
 variable "key_path" {}
-variable "bastion_host" {}
-variable "bastion_user" {}
 
 resource "aws_security_group" "nat" {
   name        = "${var.name}"
@@ -32,45 +30,33 @@ resource "aws_security_group" "nat" {
   }
 }
 
-resource "template_file" "nat" {
-  filename = "${path.module}/nat.conf.tpl"
+//resource "template_file" "nat" {
+//  filename = "${path.module}/nat.conf.tpl"
+//
+//  vars {
+//    vpc_cidr = "${var.vpc_cidr}"
+//  }
+//}
 
-  vars {
-    vpc_cidr = "${var.vpc_cidr}"
-  }
-}
-
-module "ami" {
-  source        = "github.com/terraform-community-modules/tf_aws_ubuntu_ami/ebs"
-  instance_type = "${var.instance_type}"
-  region        = "${var.region}"
-  distribution  = "trusty"
+module "nat_ami" {
+  source = "github.com/atsaki/tf_aws_nat_ami"
+  region = "${var.region}"
 }
 
 resource "aws_instance" "nat" {
-  ami           = "${module.ami.ami_id}"
+  ami           = "${module.nat_ami.ami_id}"
   count         = "${length(split(",", var.public_subnets))}" # Comment out count to only have 1 NAT
   # count         = "${length(split(",", var.subnet_ids))}" # This doesn't work if the subnets are not yet created
   instance_type = "${var.instance_type}"
   key_name      = "${var.key_name}"
   subnet_id     = "${element(split(",", var.subnet_ids), count.index)}"
-  user_data     = "${template_file.nat.rendered}"
+  # user_data     = "${template_file.nat.rendered}"
 
+  associate_public_ip_address = true
   source_dest_check      = false
   vpc_security_group_ids = ["${aws_security_group.nat.id}"]
 
   tags { Name = "${var.name}.${count.index+1}" }
-
-  provisioner "remote-exec" {
-    inline = ["while sudo pkill -0 cloud-init 2>/dev/null; do sleep 2; done"]
-    connection {
-      user         = "ubuntu"
-      host         = "${self.private_ip}"
-      key_file     = "${var.key_path}"
-      bastion_host = "${var.bastion_host}"
-      bastion_user = "${var.bastion_user}"
-    }
-  }
 }
 
 output "instance_ids" { value = "${join(",", aws_instance.nat.*.id)}" }
